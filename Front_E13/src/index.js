1b41ffa3cceabd8659eeced0c55b6d252ee5de8c
Doszhan <doszhan.m@mail.ru>
2021-10-14 19:42:27 +0600

    husky

--- Front_E13/src/index.js
+++ Front_E13/src/index.js
@@ -1,6 +1,8 @@
-const heading = document.createElement('h1')
-heading.textContent = 'Как интересно!'
+const heading = document.createElement('h1');
+heading.textContent = 'Как интересно!';
 
 // добавляем заголовок в DOM
-const root = document.querySelector('#root')
-root.append(heading)
+const root = document.querySelector('#root');
+root.append(heading);
+
+console.log(1);
